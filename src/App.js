effa5aaf8a3138e201b192b74c47d75ee2b696d9
pritammaity123456 <pritammaity123456@gmail.com>
2018-07-11 19:08:37 +0530

    demo changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-//import { Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { Route, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
